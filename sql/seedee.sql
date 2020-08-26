78f90f2ad4a754ec5f1e82162d80aa9b47501ef5
Anthony Belcastro <belcastt@live.com>
2020-08-26 00:13:09 -0700

    lead number changes from names

--- sql/seedee.sql
+++ sql/seedee.sql
@@ -3,11 +3,11 @@ use cms_db;
 INSERT INTO employee
     (first_Name, lastName, manager_id, role_id)
 VALUES
-    ("john", "doe", "ashley rodriguez", 10),
-    ("mike", "chan", "john doe", 11),
+    ("john", "doe", 3, 10),
+    ("mike", "chan", 11, 11),
     ("ashley", "rodriguez", null, 20),
-    ("kevin", "tupik", "ashley rodriguez", 21),
+    ("kevin", "tupik", 3, 21),
     ("malia", "brown", null, 31),
     ("sarah", "lourd", null, 40),
-    ("tom", "allen", "sarah lourd", 41),
-    ("christian", "eckenrode", "mike chan", 20);
+    ("tom", "allen", 6, 41),
+    ("christian", "eckenrode", 12, 20);
